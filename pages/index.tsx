515732f576b1fe162611c7f2f2d901e72f707b83
ASHIK AHMED <ashikahmed121@gmail.com>
2022-10-10 21:01:55 +0600

    skills section done

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,8 +1,11 @@
 import type { NextPage } from 'next'
 import Head from 'next/head'
 import About from '../components/About'
+import Experiences from '../components/Experiences'
 import Header from '../components/Header'
 import Hero from '../components/Hero'
+import Projects from '../components/Projects'
+import Skills from '../components/Skills'
 
 const Home: NextPage = () => {
   return (
@@ -19,6 +22,18 @@ const Home: NextPage = () => {
 
       <section id='about' className='snap-center'>
         <About />
+      </section>
+
+      <section id='experience' className='snap-center'>
+        <Experiences />
+      </section>
+
+      <section id='skills' className='snap-start'>
+        <Skills />
+      </section>
+
+      <section id='projects' className=''>
+        <Projects />
       </section>
 
     </div>
