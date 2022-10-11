0429abfd29068e0ec44c45e3c0ba27877c21f801
ASHIK AHMED <ashikahmed121@gmail.com>
2022-10-11 20:29:09 +0600

    design issue fixed

--- components/ExperienceCard.tsx
+++ components/ExperienceCard.tsx
@@ -5,7 +5,7 @@ type Props = {}
 
 export default function ExperienceCard({ }: Props) {
     return (
-        <article className='flex flex-col rounded-lg items-center space-y-3 flex-shrink-0 w-[500px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] px-10 py-3 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 relative top-20'>
+        <article className='flex flex-col rounded-lg space-y-3 flex-shrink-0 w-[400px] md:w-[600px] xl:w-[900px] snap-center bg-[#292929] md:px-10 py-3 hover:opacity-100 opacity-40 cursor-pointer transition-opacity duration-200 relative top-20'>
             <div className='flex'>
                 <motion.img
                     initial={{
@@ -22,12 +22,12 @@ export default function ExperienceCard({ }: Props) {
                     viewport={{
                         once: true
                     }}
-                    className='w-32 h-32 rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center'
+                    className='w-32 h-32 p-2 md:rounded-full xl:w-[200px] xl:h-[200px] object-cover object-center'
                     src="https://avatars.githubusercontent.com/u/30996446?v=4" alt="" />
 
                 <div className='px-0 md:px-10'>
-                    <h4 className='text-4xl font-light'>Support Engineer</h4>
-                    <p className='font-bold text-2xl mt-1'>Infozillion Teletech BD LTD.</p>
+                    <h4 className='text-xl md:text-4xl font-light'>Support Engineer</h4>
+                    <p className='font-bold text-lg md:text-2xl mt-1'>Infozillion Teletech BD LTD.</p>
                     <div className='flex space-x-2 my-2'>
                         <img className='w-10 h-10 rounded-full' src="https://ashik-ahmed.netlify.app/static/media/vscode.a2ea685afa8a93556564.png" alt="" />
                         <img className='w-10 h-10 rounded-full' src="https://ashik-ahmed.netlify.app/static/media/vscode.a2ea685afa8a93556564.png" alt="" />
@@ -37,7 +37,7 @@ export default function ExperienceCard({ }: Props) {
                 </div>
             </div>
             <div>
-                <h4 className='text-left text-3xl font-semibold underline decoration-[#F7AB0A]/50 py-3'>Key Responsibilities:</h4>
+                <h4 className='text-left text-xl font-semibold underline decoration-[#F7AB0A]/50 py-3'>Key Responsibilities:</h4>
                 <ul className='list-disc space-y-1 ml-5 text-lg'>
                     <li>Summary Points Summary Points Summary Points Summary </li>
                     <li>Summary Points Summary Summary Points Summary Points</li>
--- components/Experiences.tsx
+++ components/Experiences.tsx
@@ -16,8 +16,8 @@ export default function Experiences({ }: Props) {
             transition={{
                 duration: 1.5
             }}
-            className='h-screen flex relative flex-col text-left md:flex-row max-w-full px-10 py-10 justify-evenly mx-auto items-center'>
-            <h3 className='absolute top-20 uppercase tracking-[20px] text-gray-500 text-2xl'>Experience</h3>
+            className='h-screen flex relative flex-col text-left md:flex-row max-w-full p-10 justify-evenly mx-auto items-center'>
+            <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-500 text-2xl'>Experience</h3>
 
             <div className='w-full flex space-x-5 overflow-x-scroll overflow-y-hidden p-10 pb-28 snap-x snap-mandatory  scrollbar-track-gray-400/20 scrollbar-thumb-[#F7AB0A]/80 scrollbar-thin'>
                 <ExperienceCard />
